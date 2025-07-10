970821fd72c6b21708c3f96ee0c51c6233119482
Akkshatt <111251147+Akkshatt@users.noreply.github.com>
2025-07-10 21:50:42 +0530

    Update snippet box: modified handlers, added middleware, templates

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"flag"
+	"html/template"
      "log"
 	"net/http"
 	"os"
@@ -15,6 +16,7 @@ type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
 	snippets *models.SnippetModel
+	templateCache map[string]*template.Template
 }
 
 func main() {
@@ -33,19 +35,28 @@ func main() {
 
 	defer db.Close()
 
+
+	
+	 templateCache ,err := newTemplateCache()
+	 if err != nil{
+		errorLog.Fatal(err)
+	 }
+
+
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
 		snippets: &models.SnippetModel{DB: db},
+		templateCache: templateCache,
 	}
 
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	// mux := http.NewServeMux()
+	// fileServer := http.FileServer(http.Dir("./ui/static/"))
 
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	// mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	// mux.HandleFunc("/", app.home)
+	// mux.HandleFunc("/snippet/view", app.snippetView)
+	// mux.HandleFunc("/snippet/create", app.snippetCreate)
 
 	srv := &http.Server{
 		Addr:     *addr,
--- go.mod
+++ go.mod
@@ -2,6 +2,10 @@ module github.com/Akkshatt/go_snippet_box
 
 go 1.23.1
 
-require github.com/go-sql-driver/mysql v1.8.1
+require (
+	github.com/go-sql-driver/mysql v1.8.1
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/justinas/alice v1.2.0
+)
 
 require filippo.io/edwards25519 v1.1.0 // indirect
